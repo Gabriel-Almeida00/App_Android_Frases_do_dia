0d86a3bbdc451c752914b595ff38fd3e41504038
gabriel <gaamdal557@gmail.com>
2023-04-12 13:49:01 -0300

    finalzando app

--- app/src/main/java/com/example/frasesdodia/MainActivity.java
+++ app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View v){
+        String[] frases = {
+                "Frase 1",
+                "Frase 2",
+                "Frase 3",
+                "Frase 4"
+        };
+        int numero = new Random().nextInt(4);
+        TextView texto = findViewById(R.id.txtResultado);
+        texto.setText( frases[numero] );
+    }
+
 }
